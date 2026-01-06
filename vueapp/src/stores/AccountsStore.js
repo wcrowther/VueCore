d327e4a60218c85f48180c308d4f5e2c1b4dd601
wcrowther <wcrowther22@gmail.com>
2026-01-06 15:47:21 -0500

    Converted Account Store to composition API style code.

--- vueapp/src/stores/AccountsStore.js
+++ vueapp/src/stores/AccountsStore.js
@@ -1,138 +1,141 @@
 
-const toastStore  = useToastStore()
+// Uses Composition Api-style syntax
 
-export const useAccountsStore = defineStore('AccountsStore',
+export const useAccountsStore = defineStore('AccountsStore', () =>
 {
-    state: () => 
-    ({
-        accountsPager:      new PagerModel(new SearchForAccount()),
-        accountsList:       [],
-        account:            {},
-        uneditedAccount:    {},
-        detailAccountId:    0
-    }),
-    getters:
+    const toastStore = useToastStore()
+
+    // STATE ------------------------------------------------------------------
+
+    const accountsPager   = ref(new PagerModel(new SearchForAccount()))
+    const accountsList    = ref([])
+    const account         = ref({})
+    const uneditedAccount = ref({})
+    const detailAccountId = ref(0)
+
+    // GETTERS ----------------------------------------------------------------
+
+    const accountIsDirty = computed(() =>
+        JSON.stringify(account.value) !== JSON.stringify(uneditedAccount.value)
+    )
+
+    // ACTIONS ----------------------------------------------------------------
+
+    function addNewAccount()
     {
-        accountIsDirty: (state) =>  JSON.stringify(state.account) !== JSON.stringify(state.uneditedAccount)  
-    },
-    actions:
+        account.value = new AccountModel()
+    }
+
+    async function getAllAccounts()
     {
-        addNewAccount()
+        try
         {
-            this.account   = new AccountModel()
-        },
-        async getAllAccounts ()
-        {
-            try 
+            const response = await apiGet(`/accounts/getAllAccounts`)
+            if (response.success)
             {
-                const response = await apiGet(`/accounts/getAllAccounts`)                
-                if(response.success) 
-                {    
-                    this.accountsList   = response.data.Result.ListItems   
-                }
+                accountsList.value = response.data.Result.ListItems
             }
-            catch (err) { toastStore.showError(err.message) }
-        },
-        async getPagedAccounts (pager)
+        }
+        catch (err)
         {
-            try 
-            {
-                console.log('--- Get AccountList From Server') // logJson('getPagedAccounts request', JSON.stringify(pager))
-                const response = await apiPost(`/accounts/getPagedAccounts`, pager)
-                
-                if(response.success) 
-                {
-                    this.accountsPager = PagerModel.fromJson(response.data.Result.Pager, () => new SearchForAccount()) 
-                    this.accountsList  = response.data.Result.ListItems.map(item => Object.assign(new AccountModel, item))
-
-                    // OLD: this.accountsPager  = Object.assign(new PagerModel(new SearchForAccount(), response.data.Result.Pager)
-                    // OLD: this.accountsList   = response.data.Result.ListItems   
-                }
-            }
-            catch (err){ toastStore.showError(err.message) }
-        },
-        async getAccountDetailData (accountId)
-        {
-            try 
-            {
-                if(accountId && accountId > 0)
-                {
-                    console.log(`------- Get AccountDetail ${accountId} From Server`)
-                    const result = await apiGet(`/accounts/getAccountById/${accountId}`)
-                    
-                    if(result.success)
-                    {
-                        this.account         = result.data.Result
-                        this.uneditedAccount = structuredClone(toRaw(this.account))
-                        return
-                    }
-                }
-
-                this.account            = new AccountModel()
-                this.uneditedAccount    = new AccountModel()
-            } 
-            catch (err) { toastStore.showError(err.message) } 
-        },
-        async saveAccount ()
-        {
-            try 
-            {
-                const result = await apiPost(`/accounts/saveAccount/`, this.account)
-
-                if(result.success) 
-                {
-                    this.account           = result.data.Result
-                    this.uneditedAccount   = structuredClone(toRaw(this.account ))
-
-                    toastStore.showSuccess('Account Saved Successfully.')
-                }
-            } 
-            catch (err){ toastStore.showError(err.message) }
-        },
-        async resetAccount ()
-        {
-            this.account = Object.assign(new AccountModel(), this.uneditedAccount)
+            toastStore.showError(err.message)
         }
     }
+
+    async function getPagedAccounts(pager)
+    {
+        try
+        {
+            console.log('--- Get AccountList From Server')
+            const response = await apiPost(`/accounts/getPagedAccounts`, pager)
+
+            if (response.success)
+            {
+                accountsPager.value =
+                    PagerModel.fromJson(response.data.Result.Pager, () => new SearchForAccount())
+
+                accountsList.value =
+                    response.data.Result.ListItems.map(item =>
+                        Object.assign(new AccountModel(), item)
+                    )
+            }
+        }
+        catch (err)
+        {
+            toastStore.showError(err.message)
+        }
+    }
+
+    async function getAccountDetailData(accountId)
+    {
+        try
+        {
+            if (accountId && accountId > 0)
+            {
+                console.log(`------- Get AccountDetail ${accountId} From Server`)
+                const result = await apiGet(`/accounts/getAccountById/${accountId}`)
+
+                if (result.success)
+                {
+                    account.value         = result.data.Result
+                    uneditedAccount.value = structuredClone(toRaw(account.value))
+                    return
+                }
+            }
+
+            account.value         = new AccountModel()
+            uneditedAccount.value = new AccountModel()
+        }
+        catch (err)
+        {
+            toastStore.showError(err.message)
+        }
+    }
+
+    async function saveAccount()
+    {
+        try
+        {
+            const result = await apiPost(`/accounts/saveAccount/`, account.value)
+
+            if (result.success)
+            {
+                account.value         = result.data.Result
+                uneditedAccount.value = structuredClone(toRaw(account.value))
+
+                toastStore.showSuccess('Account Saved Successfully.')
+            }
+        }
+        catch (err)
+        {
+            toastStore.showError(err.message)
+        }
+    }
+
+    function resetAccount()
+    {
+        account.value = Object.assign(new AccountModel(), uneditedAccount.value)
+    }
+
+    // EXPOSE PUBLIC API ------------------------------------------------
+
+    return {
+        // state
+        accountsPager,
+        accountsList,
+        account,
+        uneditedAccount,
+        detailAccountId,
+
+        // getters
+        accountIsDirty,
+
+        // actions
+        addNewAccount,
+        getAllAccounts,
+        getPagedAccounts,
+        getAccountDetailData,
+        saveAccount,
+        resetAccount
+    }
 })
-
-
-
-// =======================================================================================
-// ALTERNATE SYNTAX
-// =======================================================================================
-// {
-//     console.log('getPagedAccounts: ') 
-//     
-//     this.isBusy  = true
-//     let queryUrl = `${baseApiUrl}/accounts/getPagedAccounts`
-// 
-//     axios.post(queryUrl, pager, axiosConfig) 
-//     .then((result) => 
-//     {
-//         this.accountsPager    = Object.assign(new PagerModel(), result.data.pager)
-//         this.accountsList     = result.data.listItems // NOTE: casing of listItems  
-// 
-//         console.log(JSON.parse(JSON.stringify(this.accountsPager))) 
-//         console.log(JSON.parse(JSON.stringify(this.accountsList))) 
-//     })
-//     .catch((err) => 
-//     {
-//         console.error(err) 
-//         this.error = err
-//     })
-//     .finally(() => { this.isBusy = false })
-// },
-// =======================================================================================
-
-// WITH DEBUGGING
-// getters:
-// {
-//     accountIsDirty: (state)  => 
-//     {   
-//          let dirty = JSON.stringify(state.account) !== JSON.stringify(state.uneditedAccount); 
-//          console.log(`accountIsDirty: ${dirty} account: `, 
-//          JSON.stringify(state.account.AccountName), 'uneditedAccount: ', JSON.stringify(state.uneditedAccount.AccountName) ); 
-//          return dirty 
-//     }          
-// }
