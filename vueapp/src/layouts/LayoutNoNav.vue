5bde452f18a24f0e16177dcd6083943db62ce2f5
wcrowther <wcrowther22@gmail.com>
2026-01-10 01:34:55 -0500

    Added LayoutNoNave

--- vueapp/src/layouts/LayoutNoNav.vue
+++ vueapp/src/layouts/LayoutNoNav.vue
@@ -30,7 +30,7 @@
 			class="fixed top-0 bottom-0 left-0 right-0 bg-gradient-back">
 		</div>
 
-		<div class="main-width mb-10 relative z-0 h-full">
+		<div class="main-width mx-auto mb-10 relative z-0 h-full">
 
 			<!-- Debugging variable here if needed
 			<div class="text-white p-3">layoutEscapeKeyOn: {{ layoutEscapeKeyOn }}</div> -->
