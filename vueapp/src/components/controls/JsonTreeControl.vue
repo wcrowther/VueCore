524c5dab35a81d106beb7c2ff344cb8b1fdb610a
wcrowther <wcrowther22@gmail.com>
2026-03-06 00:02:10 -0500

    Simplified JsonTreeControl, made isOpen a viewmodel.

--- vueapp/src/components/controls/JsonTreeControl.vue
+++ vueapp/src/components/controls/JsonTreeControl.vue
@@ -2,20 +2,17 @@
 
 	const toastStore = useToastStore()
 
+	const isOpen = defineModel('isOpen', { type: Boolean, default: false })
+
 	const props = defineProps(
 	{
 		json: { type: [Object, Array, String, Number, Boolean, null], required: true },
 		label: { type: [Object, Array, String, Number, Boolean, null], default: "" },
-		isOpen: { type: Boolean, default: true }
 	})
 	
     const { copy }   = useClipboard()
 
-	const formattedOpen = ref(props.isOpen)
-	watch(() => props.isOpen, (v) => formattedOpen.value = v)
-
 	const showRawJson 	= ref(false)	
-	const isRawOpen 	= ref(false)	
 	const toggleView 	= () =>  showRawJson.value = !showRawJson.value
 
 	const jsonToClipboard = () => 
@@ -48,20 +45,28 @@
 
 		<div v-if="showRawJson" class="border-b-[3px] border-b-color-primary">
 			<div class="bg-transparent border-t-[3px] font-bold select-none flex items-center border-color-primary">
-				<RotateButton v-model="isRawOpen" icon="material-symbols-light:play-arrow" />
+				<RotateButton v-model="isOpen" icon="material-symbols-light:play-arrow" />
 				<div class="p-1">{{ label }}
 					<span class="text-gray-400 ml-1">- Raw Json View</span>
 				</div> 
 			</div>
 			
-			<pre v-show="isRawOpen" class="ml-[10px] mt-0 pt-2 border-t border-t-gray-300 "
+			<pre v-show="isOpen" class="ml-[10px] mt-0 pt-2 border-t border-t-gray-300 "
 				>{{ JSON.stringify(json, null, 5) }}
 			</pre>
 		</div>
 
 		<JsonFormatted v-else 
-			:json="json" :label="label" v-model:isOpen="formattedOpen" />
+			:json="json" :label="label" v-model:isOpen="isOpen" />
 
 	</div>
 
 </template>
+
+<!-- Usage: 
+
+    <JsonTreeControl v-if="showJsonEntities" :json="user" class="w-full" 
+        label="User Detail Json" :isOpen="false" />
+
+	<JsonTreeControl :label="jsonSource.name" :json="jsonSource.json" :showRawJson="false" />
+-->
